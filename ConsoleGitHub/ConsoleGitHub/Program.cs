f24adc3daffd414a1cd21ac36494046fb0e7ace7
MartoVbos <107068154+MartoVbos@users.noreply.github.com>
2022-06-07 13:53:40 -0400

    Add files via upload

--- ConsoleGitHub/ConsoleGitHub/Program.cs
+++ ConsoleGitHub/ConsoleGitHub/Program.cs
@@ -10,29 +10,37 @@ namespace ConsoleGitHub
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Ingrese num 1:");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese num 2:");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("MENU");
+                Console.WriteLine("1.Sumar");
+                Console.WriteLine("2.Restar");
+                Console.WriteLine("3.Multiplicar");
+                string opc = Console.ReadLine();
+                if (opc == "1")
+                {
+                    Console.WriteLine(" {0} + {1} = {2} ", num1, num2, num1 + num2);
+                }
+                if (opc == "2")
+                {
+                    Console.WriteLine(" {0} - {1} = {2} ", num1, num2, num1 - num2);
+                }
+                if (opc == "3")
+                {
+                    Console.WriteLine(" {0} * {1} = {2} ", num1, num2, num1 * num2);
+                }
+            }
+            catch (Exception ex)
+            {
 
-            Console.WriteLine("Ingrese num 1:");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese num 2:");
-            int num2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("MENU");
-            Console.WriteLine("1.Sumar");
-            Console.WriteLine("2.Restar");
-            Console.WriteLine("3.Multiplicar");
-            string opc = Console.ReadLine();
-            if (opc == "1")
-            {
-                Console.WriteLine(" {0} + {1} = {2} ",num1,num2,num1 + num2);
+                Console.WriteLine("Error, " + ex.Message);
             }
-            if (opc == "2")
-            {
-                Console.WriteLine(" {0} - {1} = {2} ", num1, num2, num1 - num2);
-            }
-            if (opc == "3")
-            {
-                Console.WriteLine(" {0} * {1} = {2} ", num1, num2, num1 * num2);
-            }
+            
+            
             Console.ReadKey();
         }
     }
