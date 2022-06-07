d4835730036318e2e26f41ef25abd6d6f82c80e8
MartoVbos <107068154+MartoVbos@users.noreply.github.com>
2022-06-07 13:48:00 -0400

    Add files via upload

---
+++ ConsoleGitHub/ConsoleGitHub/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese num 1:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese num 2:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("MENU");
+            Console.WriteLine("1.Sumar");
+            Console.WriteLine("2.Restar");
+            Console.WriteLine("3.Multiplicar");
+            string opc = Console.ReadLine();
+            if (opc == "1")
+            {
+                Console.WriteLine(" {0} + {1} = {2} ",num1,num2,num1 + num2);
+            }
+            if (opc == "2")
+            {
+                Console.WriteLine(" {0} - {1} = {2} ", num1, num2, num1 - num2);
+            }
+            if (opc == "3")
+            {
+                Console.WriteLine(" {0} * {1} = {2} ", num1, num2, num1 * num2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
